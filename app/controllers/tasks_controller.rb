24d102ed64ebd827a0d6f80f1300a2aba18d8871
ren.shibuya <ren.hibuyas@gmail.com>
2020-08-12 12:29:11 +0000

    add colum

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,63 @@
+class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    def index
+        @tasks = Task.all
+    end
+    
+    def show
+       
+    end
+    
+    def new
+        @task = Task.new
+    end
+    
+    def create
+        @task = Task.new(task_params)
+        
+        if @task.save
+            flash[:success] = "タスクが追加されました"
+            redirect_to @task
+        else
+            flash.now[:danger] = "タスクが追加できませんでした"
+            render :new
+        end
+    end
+    
+    def edit
+       
+    end
+    
+    def update
+     
+        
+        if @task.update(task_params)
+            flash[:success] = "タスクは編集されました"
+            redirect_to @task
+        else
+            flash.now[:danger] = "タスクは編集できませんでした"
+            render :edit
+        end
+            
+    end
+    
+    def destroy
+       
+        @task.destroy
+        
+        flash[:success] = "タスクは削除されました"
+        redirect_to tasks_url
+    end
+    
+    private 
+    
+    def task_params
+        params.require(:task).permit(:content, :status)
+    end
+    
+    def set_task
+        @task = Task.find(params[:id])
+    end
+    
+    
+end
